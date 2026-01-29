bcfb18379c1de67a781a60a64f0d0a012f27cf88
Rupa <rupasindhu01@gmail.com>
2026-01-29 14:27:55 -0800

    test scm pooling

--- src/main/java/com/rupa/example/devops_demo/HelloController.java
+++ src/main/java/com/rupa/example/devops_demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello from the new DevOps pipeline!";
+        return "Hello from the new DevOps pipeline - Updated!";
     }
 }
